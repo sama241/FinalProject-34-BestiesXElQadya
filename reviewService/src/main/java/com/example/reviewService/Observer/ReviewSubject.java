92313807334b514ff0bd188c6e22d530e6e88bfa
yasmineldiasty <yasmineldiasty3@gmail.com>
2025-04-28 17:29:59 +0200

    Observer

--- reviewService/src/main/java/com/example/reviewService/Observer/ReviewSubject.java
+++ reviewService/src/main/java/com/example/reviewService/Observer/ReviewSubject.java
@@ -1,4 +1,9 @@
 package com.example.reviewService.Observer;
 
+import java.util.Observer;
+
 public interface ReviewSubject {
+    void registerObserver(Observer observer);
+    void removeObserver(Observer observer);
+    void notifyObservers();
 }
