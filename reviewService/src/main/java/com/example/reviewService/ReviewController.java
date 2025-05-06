3a0bdc5c5c977de95575d80f1011c3adca859216
yasmineldiasty <yasmineldiasty3@gmail.com>
2025-05-07 00:47:37 +0200

    Last

--- reviewService/src/main/java/com/example/reviewService/ReviewController.java
+++ reviewService/src/main/java/com/example/reviewService/ReviewController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/reviews")
-public class ReviewController {
+public class gitReviewController {
 
     private final ReviewService reviewService;
 
