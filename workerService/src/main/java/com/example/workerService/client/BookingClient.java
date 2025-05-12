760477dcffce52727e26cb7bbbc1893d0caf5067
Rotanaahmeddd <rotanaabdalrahman110@gmail.com>
2025-05-12 21:18:19 +0300

    rotafeignnnn

---
+++ workerService/src/main/java/com/example/workerService/client/BookingClient.java
@@ -0,0 +1,14 @@
+package com.example.workerService.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@FeignClient(name = "booking-service", url = "http://booking-service:8083/bookings")
+public interface BookingClient {
+
+    @GetMapping("/worker/{workerId}")
+    List<Map<String, Object>> getBookingsByWorkerId(@PathVariable("workerId") String workerId);
+}
